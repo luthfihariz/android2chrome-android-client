592f469bc7cfc7b2c3e5e7f1099c15820afc8967
Luthfi Hariz <lavalyst@gmail.com>
2013-04-14 09:36:31 +0100

    check and add new list working, code still messy

--- src/com/luthfihariz/android2chrome/CommonAsyncTask.java
+++ src/com/luthfihariz/android2chrome/CommonAsyncTask.java
@@ -1,13 +1,78 @@
+/*
+ * Copyright (c) 2012 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.luthfihariz.android2chrome;
 
-import android.os.AsyncTask;
+import java.io.IOException;
 
+import android.os.AsyncTask;
+import android.view.View;
+
+import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+
+/**
+ * Asynchronous task that also takes care of common needs, such as displaying
+ * progress, authorization, exception handling, and notifying UI when operation
+ * succeeded.
+ * 
+ * @author Yaniv Inbar
+ */
 abstract class CommonAsyncTask extends AsyncTask<Void, Void, Boolean> {
 
-	@Override
-	protected Boolean doInBackground(Void... params) {
-		
-		return null;
+	final MainActivity activity;
+	final com.google.api.services.tasks.Tasks client;
+	private final View progressBar;
+
+	CommonAsyncTask(MainActivity activity) {
+		this.activity = activity;
+		client = activity.service;
+		progressBar = activity.findViewById(R.id.title_refresh_progress);
 	}
 
+	@Override
+	protected void onPreExecute() {
+		super.onPreExecute();
+		activity.numAsyncTasks++;
+		progressBar.setVisibility(View.VISIBLE);
+	}
+
+	@Override
+	protected final Boolean doInBackground(Void... ignored) {
+		try {
+			doInBackground();
+			return true;
+		} catch (final GooglePlayServicesAvailabilityIOException availabilityException) {
+			activity.showGooglePlayServicesAvailabilityErrorDialog(availabilityException
+					.getConnectionStatusCode());
+		} catch (UserRecoverableAuthIOException userRecoverableException) {
+			activity.startActivityForResult(
+					userRecoverableException.getIntent(),
+					MainActivity.REQUEST_AUTHORIZATION);
+		} catch (IOException e) {
+			Utils.logAndShow(activity, Utils.TAG, e);
+		}
+		return false;
+	}
+
+	@Override
+	protected final void onPostExecute(Boolean success) {
+		super.onPostExecute(success);
+		if (0 == --activity.numAsyncTasks) {
+			progressBar.setVisibility(View.GONE);
+		}
+	}
+
+	abstract protected void doInBackground() throws IOException;
 }
--- src/com/luthfihariz/android2chrome/MainActivity.java
+++ src/com/luthfihariz/android2chrome/MainActivity.java
@@ -2,7 +2,9 @@ package com.luthfihariz.android2chrome;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
+import android.accounts.AccountManager;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
@@ -11,6 +13,8 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.android.gms.common.GooglePlayServicesUtil;
@@ -25,8 +29,9 @@ import com.google.api.services.tasks.TasksScopes;
 public class MainActivity extends Activity {
 
 	TextView urlTextView;
-	GoogleAccountCredential credentials;
+	GoogleAccountCredential credential;
 	private static final String PREF_ACCOUNT_NAME = "accountName";
+	public static final String PREF_TASKLIST_ID = "taskListId";
 	static final int REQUEST_GOOGLE_PLAY_SERVICES = 0;
 	static final int REQUEST_AUTHORIZATION = 1;
 	static final int REQUEST_ACCOUNT_PICKER = 2;
@@ -34,22 +39,25 @@ public class MainActivity extends Activity {
 	final HttpTransport transport = AndroidHttp.newCompatibleTransport();
 	final JsonFactory jsonFactory = new GsonFactory();
 
+	List<String> urlList;
+	ArrayAdapter<String> adapter;
+	int numAsyncTasks;
+
+	private ListView listView;
+
+	Intent intentReceived;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
 
-		Intent intent = getIntent();
-		String action = intent.getAction();
-		String type = intent.getType();
-
-		Log.d("Android2Chrome", "Action : " + action + " type : " + type);
-
+		// view and menu
+		setContentView(R.layout.calendarlist);
 		urlTextView = (TextView) findViewById(R.id.url_tv);
+		listView = (ListView) findViewById(R.id.list);
 
-		if (Intent.ACTION_VIEW.equals(action)) {
-			sendUrlToGmailTask(intent);
-		}
+		intentReceived = getIntent();
+		// String action = intentReceived.getAction();
 
 		// Google Accounts
 		credential = GoogleAccountCredential.usingOAuth2(this,
@@ -64,32 +72,129 @@ public class MainActivity extends Activity {
 
 	}
 
-	private void showGooglePlayAvailabilityErrorDialog(
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if (checkGooglePlayServicesAvailable())
+			haveGooglePlayServices();
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		switch (requestCode) {
+		case REQUEST_GOOGLE_PLAY_SERVICES:
+			if (resultCode == Activity.RESULT_OK) {
+				haveGooglePlayServices();
+			} else {
+				checkGooglePlayServicesAvailable();
+			}
+			break;
+		case REQUEST_AUTHORIZATION:
+			if (resultCode == Activity.RESULT_OK) {
+				Log.d(Utils.TAG, "request authorization!");
+
+				// do something
+				sendUrlToGmailTask(intentReceived);
+
+			} else {
+				chooseAccount();
+			}
+			break;
+		case REQUEST_ACCOUNT_PICKER:
+			if (resultCode == Activity.RESULT_OK && data != null
+					&& data.getExtras() != null) {
+				String accountName = data.getExtras().getString(
+						AccountManager.KEY_ACCOUNT_NAME);
+				if (accountName != null) {
+					credential.setSelectedAccountName(accountName);
+					SharedPreferences settings = getPreferences(Context.MODE_PRIVATE);
+					SharedPreferences.Editor editor = settings.edit();
+					editor.putString(PREF_ACCOUNT_NAME, accountName);
+					editor.commit();
+
+					// do something with google task api
+					sendUrlToGmailTask(intentReceived);
+
+				}
+			}
+			break;
+		}
+	}
+
+	private void haveGooglePlayServices() {
+		if (credential.getSelectedAccountName() == null) {
+			chooseAccount();
+		} else {
+			// do something with google api
+
+			sendUrlToGmailTask(intentReceived);
+		}
+	}
+
+	/** Check that Google Play services APK is installed and up to date */
+	private boolean checkGooglePlayServicesAvailable() {
+		final int connectionStatusCode = GooglePlayServicesUtil
+				.isGooglePlayServicesAvailable(this);
+		if (GooglePlayServicesUtil.isUserRecoverableError(connectionStatusCode)) {
+			showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
+			return false;
+		}
+		return true;
+	}
+
+	public void refreshView() {
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, urlList);
+		listView.setAdapter(adapter);
+	}
+
+	private void sendUrlToGmailTask(Intent intent) {
+		if (Intent.ACTION_VIEW.equals(intent.getAction())) {
+			Log.d(Utils.TAG, "Ready to sent...");
+			Uri sharedUrl = intent.getData();
+			URL url = null;
+			try {
+				url = new URL(sharedUrl.getScheme(), sharedUrl.getHost(),
+						sharedUrl.getPath());
+				urlTextView.setText(url.toString());
+			} catch (MalformedURLException e) {
+				Log.d("Android2Chrome", "unable to get the url");
+			}
+
+			if (url != null) {
+
+				SharedPreferences settings = getPreferences(Context.MODE_PRIVATE);
+				String taskListId = settings.getString(PREF_TASKLIST_ID, null);
+				Log.d(Utils.TAG, "URL is not null... & tasklist id : "
+						+ taskListId);
+				if (taskListId == null) {
+					Log.d(Utils.TAG, "It is checking...");
+					AsyncCheckTaskList.run(this);
+				}
+				
+				AsyncCreateTask.run(this);
+
+			}
+		}
+
+	}
+
+	public void showGooglePlayServicesAvailabilityErrorDialog(
 			final int connectionStatusCode) {
 		runOnUiThread(new Runnable() {
-
-			@Override
 			public void run() {
 				Dialog dialog = GooglePlayServicesUtil.getErrorDialog(
 						connectionStatusCode, MainActivity.this,
 						REQUEST_GOOGLE_PLAY_SERVICES);
 				dialog.show();
-
 			}
 		});
 	}
 
-	private void sendUrlToGmailTask(Intent intent) {
-		Log.d("Android2Chrome", "send!");
-		Uri sharedUrl = intent.getData();
-		URL url = null;
-		try {
-			url = new URL(sharedUrl.getScheme(), sharedUrl.getHost(),
-					sharedUrl.getPath());
-			urlTextView.setText(url.toString());
-		} catch (MalformedURLException e) {
-			Log.d("Android2Chrome", "unable to get the url");
-		}
-
+	private void chooseAccount() {
+		startActivityForResult(credential.newChooseAccountIntent(),
+				REQUEST_ACCOUNT_PICKER);
 	}
+
 }
